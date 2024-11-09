578d26dcdbcf119a8073aa190690f6d86f87c0e2
Manishr345 <manishr030405@gmail.com>
2024-11-09 19:30:50 +0530

    Made news cards on home page responsive

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -3,42 +3,42 @@ import './Categories.css';
 
 export default function Categories(props) {
   return (
-    props.clickCat && <div className={`w-[100%] ${props.isBurger?'block':'hidden'} h-[400px] md:h-[70px] sticky z-[100] top-0 md:flex`} style={{backgroundColor: 'rgb(18, 18, 18)'}}>
-      <div className='md:flex text-white  space-x-20'>
-        <div className='justify-end flex space-x-2 mr-52 md:mr-0'>
+    props.clickCat && <div className={`w-[100%] ${props.isBurger?'block':'hidden'} h-[350px] w-[100%] xl:h-[70px] sticky z-[100] top-0 xl:flex`} style={{backgroundColor: 'rgb(18, 18, 18)'}}>
+      <div className='xl:flex xl:flex-row flex flex-col text-white  space-x-20'>
+        <div className='justify-start flex space-x-2 pl-[75px]'>
         <div className='pt-5 pl-2'><svg xmlns="http://www.w3.org/2000/svg" height="20" width="22.5" viewBox="0 0 576 512"><path fill="#fcfcfd" d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>        
         </div>
-        <button className='pr-2 mt-3 md:mt-0 md:mb-2' onClick={props.business}>Business</button>
+        <button className='pr-2 mt-3 lg:mt-0 lg:mb-2' onClick={props.business}>Business</button>
         </div>
-        <div className=' flex space-x-2'>
+        <div className='justify-start flex  space-x-2'>
         <div className='pt-5 pl-2'><svg xmlns="http://www.w3.org/2000/svg" height="20" width="7.5" viewBox="0 0 192 512"><path fill="#ffffff" d="M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32l64 0c17.7 0 32 14.3 32 32l0 224 32 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 512c-17.7 0-32-14.3-32-32s14.3-32 32-32l32 0 0-192-32 0c-17.7 0-32-14.3-32-32z"/></svg>    
         </div>
-        <button className='pr-2 mt-3 md:mt-0 md:mb-2' onClick={props.entertainment}>Entertainment</button>
+        <button className='pr-2 mt-3 lg:mt-0 lg:mb-2' onClick={props.entertainment}>Entertainment</button>
         </div>
-        <div className=' flex space-x-2'>
+        <div className='justify-start flex space-x-2'>
         <div className='pt-5 pl-2'><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><path fill="#fcfcfd" d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
         </div>
-        <button className='pr-2 mt-3 md:mt-0 md:mb-2' onClick={props.general}>General</button>
+        <button className='pr-2 mt-3 lg:mt-0 lg:mb-2' onClick={props.general}>General</button>
         </div>
-        <div className=' flex space-x-2'>
+        <div className='justify-start flex space-x-2'>
         <div className='pt-5 pl-2'><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><path fill="#f7f7f7" d="M96 96c0-35.3 28.7-64 64-64l288 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L80 480c-44.2 0-80-35.8-80-80L0 128c0-17.7 14.3-32 32-32s32 14.3 32 32l0 272c0 8.8 7.2 16 16 16s16-7.2 16-16L96 96zm64 24l0 80c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24l0-80c0-13.3-10.7-24-24-24L184 96c-13.3 0-24 10.7-24 24zm208-8c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zM160 304c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16z"/></svg>
         </div>
-        <button className='pr-2 mt-3 md:mt-0 md:mb-2' onClick={props.health}>Health</button>
+        <button className='pr-2 mt-3 lg:mt-0 lg:mb-2' onClick={props.health}>Health</button>
         </div>
-        <div className=' flex space-x-2'>
+        <div className='justify-start flex space-x-2'>
         <div className='pt-5 pl-2'><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><path fill="#f7f7f7" d="M96 96c0-35.3 28.7-64 64-64l288 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L80 480c-44.2 0-80-35.8-80-80L0 128c0-17.7 14.3-32 32-32s32 14.3 32 32l0 272c0 8.8 7.2 16 16 16s16-7.2 16-16L96 96zm64 24l0 80c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24l0-80c0-13.3-10.7-24-24-24L184 96c-13.3 0-24 10.7-24 24zm208-8c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zM160 304c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16z"/></svg>
         </div>
-        <button className='pr-2 mt-3 md:mt-0 md:mb-2' onClick={props.science}>Science</button>
+        <button className='pr-2 mt-3 lg:mt-0 lg:mb-2' onClick={props.science}>Science</button>
         </div>
-        <div className=' flex space-x-2'>
+        <div className='justify-start flex space-x-2'>
         <div className='pt-5 pl-2'><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><path fill="#f7f7f7" d="M96 96c0-35.3 28.7-64 64-64l288 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L80 480c-44.2 0-80-35.8-80-80L0 128c0-17.7 14.3-32 32-32s32 14.3 32 32l0 272c0 8.8 7.2 16 16 16s16-7.2 16-16L96 96zm64 24l0 80c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24l0-80c0-13.3-10.7-24-24-24L184 96c-13.3 0-24 10.7-24 24zm208-8c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zM160 304c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16z"/></svg>
         </div>
-        <button className='pr-2 mt-3 md:mt-0 md:mb-2' onClick={props.sports}>Sports</button>
+        <button className='pr-2 mt-3 lg:mt-0 lg:mb-2' onClick={props.sports}>Sports</button>
         </div>
-        <div className=' flex space-x-2'>
+        <div className='justify-start flex space-x-2'>
         <div className='pt-5 pl-2'><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><path fill="#f7f7f7" d="M96 96c0-35.3 28.7-64 64-64l288 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L80 480c-44.2 0-80-35.8-80-80L0 128c0-17.7 14.3-32 32-32s32 14.3 32 32l0 272c0 8.8 7.2 16 16 16s16-7.2 16-16L96 96zm64 24l0 80c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24l0-80c0-13.3-10.7-24-24-24L184 96c-13.3 0-24 10.7-24 24zm208-8c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zM160 304c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16z"/></svg>
         </div>
-        <button className='pr-2 mt-3 md:mt-0 md:mb-2' onClick={props.technology}>Technology</button>
+        <button className='pr-2 mt-3 lg:mt-0 lg:mb-2' onClick={props.technology}>Technology</button>
         </div>
       </div>
     </div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,14 +4,14 @@ import logo from './logo.jpg';
 
 export default function Navbar(props) {
   return (
-    props.setHome && <div className={`nav bg-black flex justify-evenly ${props.isBurger?'h-[200px]':'h-[70px]'}  w-[100%] sticky z-[100] space-x-40 md:h-[70px]`}>
+    props.setHome && <div className={`nav bg-black flex justify-evenly ${props.isBurger?'h-[200px]':'h-[70px]'}  w-[100%] sticky z-[100] space-x-40 xl:h-[70px]`}>
       <div className='h-[70px] w-20'>
         <img className='h-[70px] w-20' src={logo}></img>
       </div>
-      <button className='burger absolute top-5 right-10 w-[20px] h-[15px] md:hidden' onClick={props.clicking}>
+      <button className='burger absolute top-5 right-10 w-[20px] h-[15px] xl:hidden' onClick={props.clicking}>
         <svg xmlns="http://www.w3.org/2000/svg" height="32" width="28" viewBox="0 0 448 512"><path fill="#ffffff" d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" /></svg>
       </button>
-      <div className={`resnavitem ${props.isBurger?'block':'hidden'} w-[100%] text-white md:flex md:justify-around`}>
+      <div className={`resnavitem ${props.isBurger?'block':'hidden'} w-[100%] text-white xl:flex xl:justify-around`}>
         <div className='px-3 rounded-sm home ml-3 flex space-x-2'>
           <div className='pt-5 pl-2'><svg xmlns="http://www.w3.org/2000/svg" height="20" width="22.5" viewBox="0 0 576 512"><path fill="#fcfcfd" d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" /></svg>
           </div>
@@ -30,7 +30,7 @@ export default function Navbar(props) {
         <div className='px-3 rounded-sm home ml-3 flex space-x-2'>
           <div className='pt-5 pl-2'><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><path fill="#f7f7f7" d="M96 96c0-35.3 28.7-64 64-64l288 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L80 480c-44.2 0-80-35.8-80-80L0 128c0-17.7 14.3-32 32-32s32 14.3 32 32l0 272c0 8.8 7.2 16 16 16s16-7.2 16-16L96 96zm64 24l0 80c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24l0-80c0-13.3-10.7-24-24-24L184 96c-13.3 0-24 10.7-24 24zm208-8c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zM160 304c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16z" /></svg>
           </div>
-          <button className='pr-2 mt-4 md:mb-6' onClick={props.onCat}>Categories</button>
+          <button className='pr-2 mt-4 xl:mb-6' onClick={props.onCat}>Categories</button>
         </div>
       </div>
 
